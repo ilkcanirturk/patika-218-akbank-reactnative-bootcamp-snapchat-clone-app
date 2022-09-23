85378ac4e6715f196ffb0061df16c2da650d861a
ilkcan irturk <ilkcan.irturk@outlook.com>
2022-09-24 01:01:36 +0300

    add constants for theme mode

---
+++ SnapchatClone/src/constants/dark.js
@@ -0,0 +1,12 @@
+export default {
+    type: 'dark',
+    backgroundColor: '#1b1b1b',
+    textColor: 'white',
+    textInfoColor: 'black',
+    headerBgColor: '#424242',
+    headerShadowColor: "#FFFC00",
+    tabBarBgColor: '#424242',
+    tabBarOnlineColor: '#FFFC00',
+    tabBarOnlineTintColor: '#FFFC00',
+    tabBarOfflineColor: 'gray'
+  };
---
+++ SnapchatClone/src/constants/light.js
@@ -0,0 +1,12 @@
+export default {
+    type: 'light',
+    backgroundColor: 'white',
+    textColor: 'black',
+    textInfoColor: 'black',
+    headerBgColor: '#fff59d',
+    tabBarBgColor: '#fff59d',
+    headerShadowColor: "#000000",
+    tabBarOnlineColor: '#000000',
+    tabBarOnlineTintColor: '#000000',
+    tabBarOfflineColor: 'gray'
+  };
